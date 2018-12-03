13c54e2f17d7adb2c5e3aa3ed72b922ba382eb24
Cristina Murillo <murillocam96@gmail.com>
2018-12-03 17:29:14 -0500

    added opening page and scroll feature

--- poli-facts-front/src/Components/Landing.js
+++ poli-facts-front/src/Components/Landing.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PoliticiansContainer from './PoliticiansContainer'
 import FactsContainer from './FactsContainer'
+import HeaderDisplay from './HeaderDisplay'
+import houseicon from '../imgs/house-icon.png'
 import axios from 'axios';
 
 
@@ -13,12 +15,21 @@ class Landing extends Component {
   render() {
     return (
       <div className="ui container">
-        <div className="ui huge header" id="topHeader">PoliFacts </div>
-        <div className="ui input focus"><input type="text" placeholder="Search by state or zip"></input></div>
+        <HeaderDisplay />
+        <h1 className="ui huge header" id="topHeader">PoliFacts </h1>
+        <div id="icon-cont">
+          <img src={houseicon} className ="ui medium centered image" alt="political icon"/><br/><br/>
+          <a href="#divider">
+            <i aria-hidden='true' class='grey angle double down big link icon'/>
+          </a>
+          <a name="divider"/>
+        </div>
 
         {this.callFetch}
         <br/> <br/>
-        <div className='ui divider' />
+        <div className='ui divider'/>
+       
+        <div className="ui input focus"><input type="text" placeholder="Search by state or zip"></input></div><br></br>
         <div className="ui divided equal width grid container" id="section2">
           <PoliticiansContainer />
           <FactsContainer />
