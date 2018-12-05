9169c3de4227ea57ac69292a00a934450ae7fa98
Scott Waring <scott.waring@gmail.com>
2018-12-05 11:41:27 -0500

    build out politican facts card

--- poli-facts-front/src/Components/FactsContainer.js
+++ poli-facts-front/src/Components/FactsContainer.js
@@ -1,21 +1,20 @@
 // handles logic for whether to access facts for a state or a zipcode
 import React from 'react'
-
+import PoliticianFacts from './PoliticianFacts'
+import StateFacts from './StateFacts'
 
 class FactsContainer extends React.Component {
-    
+
     render(){
-       
+
         return (
             <div >
 
                 <h2 className="ui header">Key Facts</h2>
                 <div className="ui piled segment">
-                    {console.log(this.props)}
-
-                   
+                    {this.props.info.abbrv ? <StateFacts state={this.props.info}/> : <PoliticianFacts pol={this.props.info}/>}
                 </div>
-                
+
             </div>
         )
     }
--- poli-facts-front/src/Components/PoliticiansContainer.js
+++ poli-facts-front/src/Components/PoliticiansContainer.js
@@ -1,25 +1,24 @@
-//handles logic for either displaying a list of politician icons or individual politican info 
+//handles logic for either displaying a list of politician icons or individual politican info
 import React from 'react'
 import PoliticianIcon from './PoliticianIcon'
 
-import PoliticianCard from './PoliticianCard'
 
 class PoliticiansContainer extends React.Component {
-    
-    
+
+
     render(){
         let poliList = this.props.politicians.map(poliObj => {
             return <PoliticianIcon {...poliObj} clickHandler={this.props.clickHandler}/>
         })
         return (
             <div >
-                
-                
+
+
                 <h2 className="ui header">Elected Officials</h2>
                 <div className= "ui centered cards">
                     {poliList}
                 </div>
-                
+
             </div>
         )
     }
