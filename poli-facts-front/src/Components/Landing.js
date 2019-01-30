c1e533cf1df1436fe30746b5c89433dd5344acec
Scott Waring <scott.waring@gmail.com>
2019-01-29 20:15:44 -0500

    added state object

--- poli-facts-front/src/Components/Landing.js
+++ poli-facts-front/src/Components/Landing.js
@@ -56,13 +56,65 @@ class Landing extends Component {
   }
 
   hoverHandler = (event) => {
+    let states = { AZ: 'Arizona',
+      AL: 'Alabama',
+      AK: 'Alaska',
+      AR: 'Arkansas',
+      CA: 'California',
+      CO: 'Colorado',
+      CT: 'Connecticut',
+      DC: 'District of Columbia',
+      DE: 'Delaware',
+      FL: 'Florida',
+      GA: 'Georgia',
+      HI: 'Hawaii',
+      ID: 'Idaho',
+      IL: 'Illinois',
+      IN: 'Indiana',
+      IA: 'Iowa',
+      KS: 'Kansas',
+      KY: 'Kentucky',
+      LA: 'Louisiana',
+      ME: 'Maine',
+      MD: 'Maryland',
+      MA: 'Massachusetts',
+      MI: 'Michigan',
+      MN: 'Minnesota',
+      MS: 'Mississippi',
+      MO: 'Missouri',
+      MT: 'Montana',
+      NE: 'Nebraska',
+      NV: 'Nevada',
+      NH: 'New Hampshire',
+      NJ: 'New Jersey',
+      NM: 'New Mexico',
+      NY: 'New York',
+      NC: 'North Carolina',
+      ND: 'North Dakota',
+      OH: 'Ohio',
+      OK: 'Oklahoma',
+      OR: 'Oregon',
+      PA: 'Pennsylvania',
+      RI: 'Rhode Island',
+      SC: 'South Carolina',
+      SD: 'South Dakota',
+      TN: 'Tennessee',
+      TX: 'Texas',
+      UT: 'Utah',
+      VT: 'Vermont',
+      VA: 'Virginia',
+      WA: 'Washington',
+      WV: 'West Virginia',
+      WI: 'Wisconsin',
+      WY: 'Wyoming'
+    }
     let selectState = event.target.dataset.name
     if (selectState !== undefined) {
       this.setState({
-        stateName: selectState
+        stateName: states[selectState]
       })
     }
-  
+
   }
 
 
@@ -96,7 +148,7 @@ class Landing extends Component {
               <a href="#divider"><button class="ui left floated button" id="sticky">Back to Map</button></a>
             </ScrollableAnchor>
             <div/>
-            
+
             <FactsContainer state={this.state.info.state.name} info={this.state.facts.length === 0 ? this.state.info.state : this.state.facts} /><br/>
             <div className='ui divider'/>
               {this.state.info.governor && <GovernorIcon {...this.state.info.governor} clickHandler={this.clickHandler}/>}
